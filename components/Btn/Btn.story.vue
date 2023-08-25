bacc506a612f0c35de9783df1e353bab9b8cb43a
Kylian, Jakub <jacob.kylian@gmail.com>
2023-08-25 15:21:05 +0200

    init

---
+++ components/Btn/Btn.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Story title="Button" :layout="{ type: 'single', iframe: false }">
+    <Btn>
+      Test
+    </Btn>
+  </Story>
+</template>
